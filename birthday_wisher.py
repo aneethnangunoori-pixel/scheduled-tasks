d1f01b875e15cca2851cff8438e25e7f1c825fea
aneethnangunoori-pixel <aneethnangunoori@gmail.com>
2026-06-23 20:46:24 -0700

    Update birthday_wisher.py

--- birthday_wisher.py
+++ birthday_wisher.py
@@ -11,7 +11,7 @@ import pandas
 import random
 import smtplib
 import os
-https://github.com/aneethnangunoori-pixel/scheduled-tasks/tree/main
+
 # import os and use it to get the GitHub repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_BIRTHDAY_APP_PASSWORD")
@@ -20,7 +20,7 @@ today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
