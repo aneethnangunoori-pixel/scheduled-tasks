e048697e7fb2cf089213bb210518c212e079a419
aneethnangunoori-pixel <aneethnangunoori@gmail.com>
2026-06-23 21:03:49 -0700

    Update birthday_wisher.py

--- birthday_wisher.py
+++ birthday_wisher.py
@@ -21,6 +21,9 @@ today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+print(f"Python thinks today's date tuple is: {today_tuple}")
+print(f"The dates found in your CSV file are: {list(birthdays_dict.keys())}")
+
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
