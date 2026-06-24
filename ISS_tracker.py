1c93378807c522e70f480e60e18892195a039203
aneethnangunoori-pixel <aneethnangunoori@gmail.com>
2026-06-24 12:02:55 -0700

    Update ISS_tracker.py

--- ISS_tracker.py
+++ ISS_tracker.py
@@ -2,9 +2,10 @@ import requests
 from datetime import datetime
 import smtplib
 import time
+import os
 
-MY_EMAIL = "aneethnangunoori@gmail.com"
-MY_PASSWORD = "___YOUR_PASSWORD_HERE___"
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_ISS_APP_PASSWORD")
 MY_LAT = 34.180733 # Your latitude
 MY_LONG = -118.344076 # Your longitude
 
@@ -20,6 +21,7 @@ def is_iss_overhead():
     #Your position is within +5 or -5 degrees of the iss position.
     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
         return True
+    return False
 
 
 def is_night():
@@ -34,16 +36,15 @@ def is_night():
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-    time_now = datetime.now().hour
+    time_now = datetime.utcnow().hour
 
     if time_now >= sunset or time_now <= sunrise:
         return True
 
 
 while True:
-    time.sleep(60)
-    if True:
-        connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___")
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com")
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
@@ -51,6 +52,6 @@ while True:
             to_addrs=MY_EMAIL,
             msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
         )
-
+    time.sleep(60)
 
 
