52007c72ef6afe22834963dfe01036ce6257c8e5
aneethnangunoori-pixel <aneethnangunoori@gmail.com>
2026-06-25 11:47:01 -0700

    Add rain alert functionality with Twilio integration

---
+++ rain_alert.py
@@ -0,0 +1,36 @@
+import requests
+from twilio.rest import Client
+import os
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "6d7907ae6375dafad64d6f5be3e4af8f"
+account_sid = os.environ.get("OWM_account_sid")
+auth_token = os.environ.get("OWM_auth_token")
+
+weather_parameters = {
+    "lat": 34.180733,
+    "lon": -118.344076,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_parameters)
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+        break
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_="whatsapp:+14155238886",
+        body="It's going to rain today. Remember to bring an ☔️",
+        to="whatsapp:+17473290982"
+    )
+    print(message.status)
